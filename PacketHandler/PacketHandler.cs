4b214fec33d21f0ad5214194890a61c17b8cdbb9
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-05 01:30:54 +0300

    change PacketHandler

--- PacketHandler/PacketHandler.cs
+++ PacketHandler/PacketHandler.cs
@@ -4,8 +4,6 @@ namespace OnChat.Protocol.PacketHandler;
 
 public abstract class PacketHandler<T> : IPacketHandler
 {
-    public abstract PacketId PacketId { get; }
-
     protected abstract Task Handle(T packet);
     public async Task Handle(IPacket packet) => await Handle((T)packet);
 }
