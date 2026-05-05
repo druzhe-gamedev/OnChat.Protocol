62f566af76e56c3afffe41c80addca750c16a94b
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-05 23:19:16 +0300

    add order of properties

--- Codecs/Impl/StructCodec.cs
+++ Codecs/Impl/StructCodec.cs
@@ -7,6 +7,21 @@ public class StructCodec(Type structType, PropertyInfo[] properties, Constructor
 {
     public override void Encode(BinaryWriter writer, object value)
     {
+        if(ctor != null)
+        {
+            ParameterInfo[] parameters = ctor.GetParameters();
+            properties = properties
+                         .OrderBy(p =>
+                             {
+                                 int index = Array.FindIndex(
+                                     parameters,
+                                     param => string.Equals(param.Name, p.Name, StringComparison.OrdinalIgnoreCase)
+                                 );
+                                 return index >= 0 ? index : int.MaxValue;
+                             }
+                         )
+                         .ToArray();
+        }
         foreach (PropertyInfo property in properties)
         {
             object propertyValue = property.GetValue(value)!;
@@ -18,13 +33,23 @@ public class StructCodec(Type structType, PropertyInfo[] properties, Constructor
     public override object Decode(BinaryReader reader)
     {
         List<object> args = [];
-        args.AddRange(
-            ctor != null
-                ? ctor.GetParameters().Select(parameter => Protocol.GetCodec(parameter.ParameterType).Decode(reader))
-                : properties.Select(property => Protocol.GetCodec(property.PropertyType).Decode(reader))
-        );
 
-        object instance = Activator.CreateInstance(structType, args)!;
+        if (ctor != null)
+        {
+            args.AddRange(ctor.GetParameters().Select(param => Protocol.GetCodec(param.ParameterType).Decode(reader)));
+
+            object[] argsArray = args.ToArray();
+            return Activator.CreateInstance(structType, argsArray)!;
+        }
+        
+        object instance = Activator.CreateInstance(structType)!;
+        foreach (PropertyInfo property in properties)
+        {
+            object propertyValue = Protocol.GetCodec(property.PropertyType).Decode(reader);
+
+            property.SetValue(instance, propertyValue);
+        }
+
         return instance;
     }
 }
