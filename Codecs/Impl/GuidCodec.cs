16110d61e192c2e79f9fe713cca59b1ae0337554
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-05 04:09:01 +0300

    add guid codec

---
+++ Codecs/Impl/GuidCodec.cs
@@ -0,0 +1,18 @@
+﻿namespace OnChat.Protocol.Codecs.Impl;
+
+public class GuidCodec : CodecInfo<Guid>
+{
+    public override void Encode(BinaryWriter writer, Guid value)
+    {
+        byte[] guidBytes = value.ToByteArray();
+        
+        writer.Write(16);
+        writer.Write(guidBytes);
+    }
+
+    public override Guid Decode(BinaryReader reader)
+    {
+        Guid guid = Guid.Parse(reader.ReadBytes(16));
+        return guid;
+    }
+}
