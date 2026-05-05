37029f01b1e6e86cce48913b71c341b23c4858ea
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-05 21:29:44 +0300

    add Int16 codec

---
+++ Codecs/Impl/Int16Codec.cs
@@ -0,0 +1,8 @@
+﻿namespace OnChat.Protocol.Codecs.Impl;
+
+public class Int16Codec : CodecInfo<short>
+{
+    public override void Encode(BinaryWriter writer, short value) => writer.Write(value);
+
+    public override short Decode(BinaryReader reader) => reader.ReadInt16();
+}
