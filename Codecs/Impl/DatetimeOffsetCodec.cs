dd16c8f39ff9f1741da0ae483818543e1b6dbb21
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-07 15:25:26 +0300

    add DatetimeOffsetCodec

---
+++ Codecs/Impl/DatetimeOffsetCodec.cs
@@ -0,0 +1,8 @@
+﻿namespace OnChat.Protocol.Codecs.Impl;
+
+public class DatetimeOffsetCodec : CodecInfo<DateTimeOffset>
+{
+    public override void Encode(BinaryWriter writer, DateTimeOffset value) => writer.Write(value.ToUnixTimeSeconds());
+
+    public override DateTimeOffset Decode(BinaryReader reader) => DateTimeOffset.FromUnixTimeSeconds(reader.ReadInt64());
+}
