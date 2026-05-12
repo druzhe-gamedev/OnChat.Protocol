de03be461c837d30f485e37af49e2f014b9b9c8e
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-12 03:22:22 +0300

    add authentication method

--- PacketHandler/IConnection.cs
+++ PacketHandler/IConnection.cs
@@ -4,6 +4,7 @@ namespace OnChat.Protocol.PacketHandler;
 
 public interface IConnection
 {
+    void Authenticate(Guid userId, string username);
     AuthenticationState AuthenticationState { get; }
     Task Read();
     Task Write(IPacket packet);
