1d1b097c3da886859357a6379b573370b9031cf5
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-12 18:17:49 +0300

    add wrong id packet

--- Packets/PacketId.cs
+++ Packets/PacketId.cs
@@ -14,5 +14,6 @@ public enum PacketId : byte
     TokensRotationPacket,
     BadRefreshTokenPacket,
     UnauthorizedPacket,
-    ReceiveMessagePacket
+    ReceiveMessagePacket,
+    WrongIdPacket
 }
